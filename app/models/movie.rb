1db754725561a318cb169f97ee850cf30cde0d7d
YOUR NAME <jhanov@gmail.com>
2021-07-06 15:41:06 -0500

    added has many through associations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,6 +2,7 @@ class Movie < ApplicationRecord
   validates :title, uniqueness: true
   validates :year, numericality: { greater_than: 1920 }
   validates :plot, length: { minimum: 6 }
-
+  has_many :movie_genres
   has_many :actors
+  has_many :genres, through: :movie_genres
 end
